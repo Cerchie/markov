cd81e9b87fb6c32a61393a5ef13350b32df911dc
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-15 17:17:49 -0700

    markov machine iwth var issue

---
+++ markov.js
@@ -0,0 +1,59 @@
+// // /** Textual markov chain generator */
+// const fs = require('fs');
+// const process = require('process');
+
+//HYPOTHESIS-- I CANNOT ACCESS MY TEXTARR VAR BECAUSE IT IS SCOPED TO MY CONSTRUCTOR
+// I'm working on adapting the logic here https://codepen.io/alexkramerbanjo/pen/mvPQYM to be object-oriented and I cannot work out the textArr variable
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+
+    let textArr = text.split(/[ \r\n]+/);
+    this.textArr = textArr.filter(c => c !== "");
+    this.makeChains();
+    this.makeText()
+
+  }
+  //   /** set markov chains:
+  //    *
+  //    *  for text of "the cat in the hat", chains will be
+  //    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+
+    const markovChain = {}
+    for (let i = 0; i < this.textArr.length; i++) {
+      let word = this.textArr[i].toLowerCase().replace(/[\W_]/, "")
+      if (!markovChain[word]) {
+        markovChain[word] = []
+      }
+      if (this.textArr[i + 1]) {
+        markovChain[word].push(this.textArr[i + 1].toLowerCase().replace(/[\W_]/, ""));
+      }
+    }
+  }
+  //   /** return random text from chains */
+  words = Object.keys(this.makeChains(this.textArr))
+
+  makeText() {
+
+    let word = words[Math.floor(Math.random() * words.length)]
+    let result = ''
+    for (let i = 0; i < words.length; i++) {
+      result += word + ' ';
+      let newWord = markovChain[word][Math.floor(Math.random() * markovChain[word].length)]
+      word = newWord;
+      if (!word || !markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
+    }
+    return result;
+  }
+
+}
+
+let mm = new MarkovMachine("the cat in the hat"); //instantiated
+console.log(mm)
+mm.makeText();
+
+console.log(mm.makeText());
