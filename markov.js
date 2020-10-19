c442ef49e9df3162c004afd380d4a84b783996e8
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-19 09:51:55 -0700

    first level complete

--- markov.js
+++ markov.js
@@ -12,40 +12,41 @@ class MarkovMachine {
 
     let textArr = text.split(/[ \r\n]+/);
     this.textArr = textArr.filter(c => c !== "");
+
     this.makeChains();
-    this.makeText()
+    this.makeText();
+
 
   }
   //   /** set markov chains:
   //    *  for text of "the cat in the hat", chains will be
   //    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
-
+  markovChain = {};
   makeChains = () => {
-
-    const markovChain = {}
+    // const markovChain = {}
     for (let i = 0; i < this.textArr.length; i++) {
       let word = this.textArr[i].toLowerCase().replace(/[\W_]/, "")
-      if (!markovChain[word]) {
-        markovChain[word] = []
+      if (!this.markovChain[word]) {
+        this.markovChain[word] = []
       }
       if (this.textArr[i + 1]) {
-        markovChain[word].push(this.textArr[i + 1].toLowerCase().replace(/[\W_]/, ""));
+        this.markovChain[word].push(this.textArr[i + 1].toLowerCase().replace(/[\W_]/, ""));
       }
     }
-    return markovChain;
+    return this.markovChain;
   }
   //   /** return random text from chains */
 
 
   makeText = () => {
-    words = Object.keys(this.makeChains(this.textArr))
+    let words = Object.keys(this.makeChains(this.textArr))
     let word = words[Math.floor(Math.random() * words.length)]
     let result = ''
     for (let i = 0; i < words.length; i++) {
       result += word + ' ';
-      let newWord = markovChain[word][Math.floor(Math.random() * markovChain[word].length)]
+      let newWord = this.markovChain[word][Math.floor(Math.random() * this.markovChain[word].length)]
       word = newWord;
-      if (!word || !markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
+      if (!word || !this.markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
     }
     return result;
   }
@@ -53,7 +54,6 @@ class MarkovMachine {
 }
 
 let mm = new MarkovMachine("the cat in the hat"); //instantiated
-console.log(mm)
 mm.makeText();
 
 console.log(mm.makeText());
